f78658140c7c1920832b3a8ff361ba706fd6092b
Sheviny <2187357@talnet.nl>
2025-05-07 21:02:42 +0200

    homepage begin.

---
+++ home/home.php
@@ -0,0 +1,48 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/home.css">
+  <title>Document</title>
+</head>
+<body>
+
+
+  <div class="container">
+    <!-- navbar -->
+    <div class="navbar">
+
+      <!-- logo -->
+      <div class="logo">
+        <a href="../home/home.php">
+          <img src="../logo/flyway.png" alt="logo" width="125px">
+        </a>
+      </div>
+      <!-- logo -->
+
+      <nav>
+        <ul>
+          <li><a href="#">Vluchten</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="../user/login-user.php">Account</a></li>
+        </ul>
+      </nav>
+    </div>
+    <!-- navabar -->
+
+
+  <header class="front">
+    <div class="front-overlay">
+    <h1>Welkom op <br> Amsterdam Airport</h1>
+    <p>Hier begint je avontuur ✈️</p>
+    </div>
+  </header>
+  
+</body>
+</html>
