aa72ed46f552a147e027e994744edf388d078c08
Sheviny <2187357@talnet.nl>
2025-06-11 08:53:32 +0200

    css schedule details.

--- flight_schedules/view_scheduleDetails.php
+++ flight_schedules/view_scheduleDetails.php
@@ -20,7 +20,8 @@ if (!$scheduleDetail) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <link rel="stylesheet" href="../css/home.css">
+  
+  <link rel="stylesheet" href="../css/schedule_details.css">
 </head>
 <body>
 
